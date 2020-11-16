29077fa2f79d6fa8b4ad707c4b84a7dbcf30c937
PumpkinSeed <qwer.kocka@gmail.com>
2020-11-16 08:13:51 +0000

    Add new secret_toolkit version, fixes based on that

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,8 @@
 use core::mem;
 use cosmwasm_std::Env;
-use secret_toolkit::crypto::{sha_256};
+use secret_toolkit::crypto::{sha_256, Prng};
 
-use crate::crypto::{prng, HASH_SIZE};
+use crate::crypto::{HASH_SIZE};
 use crate::state::PrivateKeyRecord;
 
 pub fn generate_api_key(seed: &[u8], env: &Env) -> String {
@@ -10,8 +10,10 @@ pub fn generate_api_key(seed: &[u8], env: &Env) -> String {
 
     let mut entropy: Vec<u8> = env.message.sender.0.as_bytes().to_vec();
     entropy.extend_from_slice(height_slice.as_ref());
-
-    "api_key_".to_string() + &base64::encode(prng(seed, &entropy, (HASH_SIZE / 2) as u32))
+    
+   let mut rng = Prng::new(seed, &entropy);
+    rng.set_word_pos((HASH_SIZE / 2) as u32);
+    "api_key_".to_string() + &base64::encode(rng.rand_bytes())
 }
 
 pub fn authenticate_request(
@@ -32,7 +34,10 @@ pub fn generate_seed(keying_material: &String) -> [u8; 32] {
 
 pub fn generate_key_id(env: &Env) -> String {
     let entropy = unsafe { mem::transmute::<u64, [u8; 8]>(env.block.height) };
-    "key_".to_string() + &base64::encode(prng(env.message.sender.0.as_bytes(), entropy.as_ref(), 0))
+
+    let mut rng = Prng::new(env.message.sender.0.as_bytes(), entropy.as_ref());
+    rng.set_word_pos(0);
+    "key_".to_string() + &base64::encode(rng.rand_bytes())
 }
 
 pub fn generate_private_key(env: &Env, seed: &[u8], entropy: &[u8]) -> [u8; 32] {
@@ -41,5 +46,8 @@ pub fn generate_private_key(env: &Env, seed: &[u8], entropy: &[u8]) -> [u8; 32] 
     let mut keying_material: Vec<u8> = env.message.sender.0.as_bytes().to_vec();
     keying_material.extend_from_slice(height_slice.as_ref());
     keying_material.extend_from_slice(entropy);
-    prng(seed, &keying_material, 0)
+   
+    let mut rng = Prng::new(seed, &keying_material);
+    rng.set_word_pos(0);
+    rng.rand_bytes()
 }
